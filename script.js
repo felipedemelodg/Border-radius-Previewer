2a308507d280ff6d6e8c3326c3364bef4f5cb0d7
felipdemelodg <felipedemelodg@gmail.com>
2022-03-22 00:06:02 -0300

    finalização

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-let botao = document.getElementById('botao')
-let css = document.getElementById('css')
-let radius = ['10px', '10px', '10px', '10px']
+let topRight = document.getElementById('top-right')
 let topLeft = document.getElementById('top-left')
-topLeft.style.backgroundColor = 'green'
-let largura = botao.clientWidth
-console.log(largura)
+let bottomRight = document.getElementById('bottom-right')
+let bottomLeft = document.getElementById('bottom-left')
+let caixa = document.getElementById('caixa')
+let cssStyle = document.getElementById('css-style')
+let copiar = document.getElementById('copiar')
 
-botao.style.borderTopLeftRadius = radius[0]
-botao.style.borderTopRightRadius = radius[1]
-botao.style.borderBottomLeftRadius = radius[2]
-botao.style.borderBottomRightRadius = radius[3]
+caixa.addEventListener('input', controleRadius, false)
+copiar.addEventListener('click', selectText)
 
-css.innerText = [
-    botao.style.borderTopLeftRadius,
-    botao.style.borderTopRightRadius,
-    botao.style.borderBottomLeftRadius,
-    botao.style.borderBottomRightRadius
-]
-let cont = 0
-window.addEventListener('mouseover', function (e) {
-    topLeft.style.left = `${cont = cont + 10}px`
-   
-})
+function selectText() {
+    navigator.clipboard.writeText(cssStyle.innerText)
+}
+
+function controleRadius() {
+  caixa.style.borderTopRightRadius = `${topRight.value}px`
+  caixa.style.borderTopLeftRadius = `${topLeft.value}px`
+  caixa.style.borderBottomRightRadius = `${bottomRight.value}px`
+  caixa.style.borderBottomLeftRadius = `${bottomLeft.value}px`
+
+
+
+  let radius = {
+    'border-top-right-radius': caixa.style.borderTopRightRadius,
+    'border-top-left-radius': caixa.style.borderTopLeftRadius,
+    'border-bottom-right-radius': caixa.style.borderBottomRightRadius,
+    'border-bottom-left-radius': caixa.style.borderBottomLeftRadius
+  }
+
+  cssStyle.innerText = [
+    `
+  ${Object.keys(radius)[0]}: ${Object.values(radius)[0]}
+  ${Object.keys(radius)[1]}: ${Object.values(radius)[1]}
+  ${Object.keys(radius)[2]}: ${Object.values(radius)[2]}
+  ${Object.keys(radius)[3]}: ${Object.values(radius)[3]}
+  `
+  ]
+}
